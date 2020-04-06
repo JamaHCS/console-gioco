c51768fd54c21ce80bfd10041246f1a704dcfbd1
JamaHCS <jama117@hotmail.com>
2020-04-06 18:37:30 -0500

    Agregado el array de tipo de datos primitivos

--- src/main/java/com/gioco/view/MainMenu.java
+++ src/main/java/com/gioco/view/MainMenu.java
@@ -9,17 +9,24 @@ import com.gioco.controller.Tools;
 
 public class MainMenu implements IVisualizable {
     private int option = 0;
+    private int optionArray[] = new int[4];
+
 
     @Override
     public void show() {
+        for (int i = 0; i < 4; i++) {
+            optionArray[i] = i;
+        }
+
+
         do {
             Tools.padding();
             Tools.printTitle("Gioco - Tienda de videojuegos");
             System.out.println("Selecciona la opción deseada.\n");
-            System.out.println("1.- Administrar Artículos");
-            System.out.println("2.- Administrar Clientes");
-            System.out.println("3.- Administrar ventas");
-            System.out.println("0.- Salir");
+            System.out.println(optionArray[1] + ".- Administrar Artículos");
+            System.out.println(optionArray[2] + ".- Administrar Clientes");
+            System.out.println(optionArray[3] + ".- Administrar ventas");
+            System.out.println(optionArray[0] + ".- Salir");
 
             option = Tools.getOption();
             Tools.validateOption(0, 3, option);
