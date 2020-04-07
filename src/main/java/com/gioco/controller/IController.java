9e8c428bc16f457041a815a40c98714884f917f7
JamaHCS <jama117@hotmail.com>
2020-04-06 19:48:20 -0500

    javadoc

--- src/main/java/com/gioco/controller/IController.java
+++
@@ -1,9 +0,0 @@
-package com.gioco.controller;
-
-
-/**
- * Interfaz hecha para establecer las clases encargadas de manejar la lógica de ciertas pantallas.
- */
-public interface IController {
-    void controller();
-}
